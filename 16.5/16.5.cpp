90aba783506ff72952b9cc147a9c133200221cb6
alex <a>
2024-03-24 12:32:06 +0400

    Добавьте файлы проекта.

---
+++ 16.5/16.5.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+int main()
+{
+    const int x = 5;
+    const int y = 5;
+  
+
+    int arr [x][y];
+
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            arr[i][j] = j + i;
+        }
+    }
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            std::cout << arr[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    int dayi = (24 % x);
+    int sum = arr[dayi][0];
+
+    for (dayi; dayi < x; dayi)
+    {
+        for (int j = 1; j < y; j++)
+        {
+            sum += arr[dayi][j];
+        }
+        break;
+    }
+    std::cout << sum;
+}
